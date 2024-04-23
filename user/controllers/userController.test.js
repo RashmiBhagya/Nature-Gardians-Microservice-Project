0def2ffc134e37d5d711a93beb5ad2b07558c099
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2024-04-23 17:52:07 +0530

    New changes for user test file

--- user/controllers/userController.test.js
+++ user/controllers/userController.test.js
@@ -1,7 +1,7 @@
 describe("userController", () => {
     describe("postUser", () => {
       test("validate user inputs", () => {});
-  //create test for validate response
+
       test("Validate response", async () => {});
     });
   });
